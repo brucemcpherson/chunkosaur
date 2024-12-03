aaac374c5667ace4de10dce76653c340673c4e73
Bruce Mcpherson <bruce@mcpher.com>
2024-12-03 10:33:05 +0000

    added handling for recursive fetcher

--- src/chunker.mjs
+++ src/chunker.mjs
@@ -6,17 +6,20 @@ export class Chunker {
   /**
    * @constructor Chunker
    * @param {function} fetcher how to fetch
+   * @param {boolean} [treatNoResultsAsDone=false] if the fetcher returns an array with zero length, treat as done, otherwise refetch
    * @param {function} [errHandler] special function to handle detected (err)=> { ... }
    * @param {*} [meta={}] any meta data to be passed through to fetcher
    * @return {Chunker}
    */
   constructor({
     fetcher,
+    treatNoResultsAsDone = false,
     errHandler = (err) => {
       throw err;
     },
     meta = {},
   }) {
+    this.treatNoResultsAsDone = treatNoResultsAsDone
     this.fetcher = fetcher;
     this.tank = [];
     this.meta = meta;
@@ -49,23 +52,35 @@ export class Chunker {
         // fetched must return done: false + values or done:true
         fetched = await fetcher({ stats: this.stats, meta: this.meta, chunker: this });
         let done = !fetched || fetched.done || typeof fetched.values === typeof undefined;
-        const values = !done && fetched.values;
-        const meta = !done && fetched.meta;
+        done = done || (this.treatNoResultsAsDone && Array.isArray(fetched) && !fetched.length)
+        // final fetched should return meta , or meta with null to retain existing meta
+        const { values, meta } = fetched;
 
         if (!done && !Array.isArray(values)) {
           return this.errHandler(
             new Error(`expected result of type array - got ${typeof values}`),
           );
         }
+        if (done && Array.isArray(values) && values.length) {
+          return this.errHandler(
+            new Error(`received done signal along with ${values.length} fetched values - only signal done when there are no more values to fetch`),
+          );
+        }
+
         // updated meta for next time
         this.meta = meta || this.meta;
+        if (done) return done
+
+        // if we received no values and got here, then it means we're not done, just that page didnt have any qualifying data
+        // so go again
+        this.stats.fetches++;
+        if (!values.length) return await fillTank()
 
         // force no more fetching
-        if (!done) {
-          this.stats.items += values.length;
-          this.stats.fetches++;
-          appendToTank(values);
-        }
+        this.stats.items += values.length;
+  
+        appendToTank(values);
+
         return done;
       } catch (err) {
         return this.errHandler(err);
--- test/test.mjs
+++ test/test.mjs
@@ -4,7 +4,7 @@ import test from 'ava';
 import delay from 'delay';
 
 // some test data
-const fix = Array.from({ length: 71 }, (_, i) => i);
+const fix = Array.from({ length: 101 }, (_, i) => i);
 
 
 test('fetcher returns undefined things and detects eof', async (t) => {
@@ -59,15 +59,62 @@ test('every item gets bulked using stats', async (t) => {
 });
 
 
-test('test meta and maxitems', async (t) => {
-  const maxItems = 51
+test('test meta and maxitems with no result', async (t) => {
+  const maxItems = 71
   const chunkSize = 1 + Math.round(Math.random() * maxItems);
   const threshold = 1 + Math.round(Math.random() * maxItems);
   const items = fix.slice()
   const outItems = [];
   // user provided fetcher - simulate an api call
   const fetcher = async ({ stats, meta }) => {
-    const { startAt = 0, limit, url, maxItems = Infinity } = meta
+    const { startAt = 0, limit,  maxItems = Infinity } = meta
+    if (stats.items >= maxItems) {
+      return {
+        done: true
+      }
+    }
+
+    // skip every 2nd to test paging with no results
+    if (stats.fetches % 2) return {
+      values: []
+    }
+    const maxLimit = Math.min (limit, maxItems - stats.items)
+    const values = await delay(Math.round(Math.random() * 10)).then(() =>
+      items.slice(stats.items + startAt, stats.items + maxLimit + startAt),
+    );
+    return {
+      values,
+      done: !values.length,
+    };
+  };
+  // user supplied flusher - this would normally be to an api - we'll simulate async with a delay
+  const flusher = async ({ values }) =>
+    delay().then(() => {
+      Array.prototype.push.apply(outItems, values);
+    });
+  const bulker = new Bulker({ flusher, threshold });
+  const chunker = new Chunker({ fetcher, meta: { startAt: 11, limit: chunkSize, maxItems } });
+  for await (const value of chunker.iterator) {
+    await delay().then(() => bulker.pusher({ values: [value] }));
+  }
+  const stats = await bulker.done();
+  const smallFix = fix.slice (chunker.meta.startAt, chunker.meta.startAt+chunker.meta.maxItems)
+  t.is(smallFix.length, outItems.length)
+  t.is(stats.pushes, stats.items);
+  t.deepEqual(outItems, smallFix);
+  t.is(outItems.length, maxItems)
+
+});
+
+test('test maxitems', async (t) => {
+  const maxItems = 71
+  const chunkSize = 1 + Math.round(Math.random() * maxItems);
+  const threshold = 1 + Math.round(Math.random() * maxItems);
+  const items = fix.slice()
+  const outItems = [];
+  // user provided fetcher - simulate an api call
+  const fetcher = async ({ stats, meta }) => {
+    const { startAt = 0, limit, maxItems = Infinity } = meta
     if (stats.items >= maxItems) {
       return {
         done: true
